5f014da75c50cfefd63d2c208f466d0eae9f1262
Sam Smart <sam.smart@bjss.com>
2018-11-25 10:16:08 +0000

    Add basic login logic in prep for API gateway

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { NgModule }       from '@angular/core';
+import { FormsModule }    from '@angular/forms';
+import { NotifierModule } from 'angular-notifier';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
-import { LoginComponent } from './login/login.component'
+import { AppComponent }     from './app.component';
+import { LoginComponent }   from './login/login.component'
 
 
 @NgModule({
@@ -14,7 +15,18 @@ import { LoginComponent } from './login/login.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NotifierModule.withConfig({
+      position: {
+        horizontal: {
+          position: 'right'
+        },
+        vertical: {
+          position: 'top'
+        }
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component }       from '@angular/core';
+import { NotifierService } from 'angular-notifier';
 
 @Component({
   selector: 'login',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  email:    string;
+  password: string;
+  notifier: NotifierService;
 
+  constructor( notifierService: NotifierService ) {
+    this.notifier = notifierService;
+  }
+
+  onSignIn() {
+    //Very basic email validation
+    if(/^(.*)@(.*)\.(.*)/.test(this.email) != true) {
+      this.notifier.notify('error', 'Invalid email address');
+      return;
+    }
+
+    //Non empty password test
+    if(!this.password) {
+      this.notifier.notify('error', 'Empty password field');
+      return;
+    }
+    
+    
+  }
 }
